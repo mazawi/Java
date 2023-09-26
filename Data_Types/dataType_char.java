a539939538532cd0a03c502cfd7d6a00c2848a3a
Mohammad Al-Azawi <azawi.gt@gmail.com>
2023-09-26 10:38:58 +0000

    v2

--- Data_Types/dataType_char.java
+++ Data_Types/dataType_char.java
@@ -1,7 +1,7 @@
 package Data_Types;
-
+// change test
 public class dataType_char {
-        public static void main(String[] args) {
+        public static void main(String[] args) { 
             char x;
             x = 'a';
             System.out.println("The variable x consists the value  "+ x);
