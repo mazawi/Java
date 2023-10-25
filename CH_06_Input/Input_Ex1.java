ba71159860d34418e03a6aa785dc569fa3b4efb7
Mohammad Al-Azawi <azawi.gt@gmail.com>
2023-10-25 20:03:03 +0000

    2

---
+++ CH_06_Input/Input_Ex1.java
@@ -0,0 +1,17 @@
+package CH_06_INPYT;
+import java.util.Scanner;
+
+public class Scanner_Ex1 
+{
+    public static void main(String[] args) 
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter an integer: ");
+            int x = input.nextInt();
+            System.out.println("You have entered: "+ x);
+
+        }
+
+            
+    
+}
