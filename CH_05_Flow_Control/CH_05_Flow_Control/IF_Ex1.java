449b8616aa27ee399199df17eb80c48151382553
Mohammad Al-Azawi <azawi.gt@gmail.com>
2023-10-25 18:50:45 +0000

    3

--- CH_05_Flow_Control/CH_05_Flow_Control/IF_Ex1.java
+++ CH_05_Flow_Control/CH_05_Flow_Control/IF_Ex1.java
@@ -2,7 +2,8 @@ package CH_05_Flow_Control;
 
 public class IF_Ex1 
 {
-        public static void main(String[] args) { 
+        public static void main(String[] args) 
+        { 
             int x = 5;
             if (x > 0)
             {
