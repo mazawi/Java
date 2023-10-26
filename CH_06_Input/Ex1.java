70b923e84270aed8c4de4a7222a1fa8109fccb37
Mohammad Al-Azawi <azawi.gt@gmail.com>
2023-10-26 05:50:24 +0000

    222

--- CH_06_Input/Ex1.java
+++ CH_06_Input/Ex1.java
@@ -1,13 +1,18 @@
 package CH_06_INPYT;
 import java.util.Scanner;
 
-public class Scanner_Ex1 
+public class Ex1 
 {
     public static void main(String[] args) 
         {
             Scanner input = new Scanner(System.in);
-            System.out.println("Enter an integer: ");
-            int x = input.nextInt();
-            System.out.println("You have entered: "+ x);
+            System.out.println("Enter N1: ");
+            int n1 = input.nextInt();
+            System.out.println("Enter N2: ");
+            int n2 = input.nextInt();
+            int r = n1+n2;
+
+            System.out.println("the sum is: "+ r);
 
         }
+}
