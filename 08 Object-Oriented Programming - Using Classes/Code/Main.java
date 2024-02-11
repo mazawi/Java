772446c180d58576bbc20ea0ae8198c2ac3476b4
mazawi <45329653+mazawi@users.noreply.github.com>
2024-02-11 19:05:07 +0400

    12

---
+++ 08 Object-Oriented Programming - Using Classes/Code/Main.java
@@ -0,0 +1,13 @@
+package Code;
+
+public class Main 
+{
+    // Example of usage
+    public static void main(String[] args) {
+        // Creating a Date object
+        Date today = new Date(31, 1, 2024);
+
+        // Displaying the date
+        System.out.println("Today's date is: " + today);
+    }
+}
