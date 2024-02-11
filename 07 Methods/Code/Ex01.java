0cb89721987a711afb0b05e11e9e5c70b181300e
mazawi <45329653+mazawi@users.noreply.github.com>
2024-02-11 20:13:02 +0400

    12

---
+++ 07 Methods/Code/Ex01.java
@@ -0,0 +1,13 @@
+public class Ex01 
+{
+     public static void sayHi()
+    {
+        System.out.println("Hi, welcome to our course");
+    }
+    public static void main(String[] args) 
+    {
+        sayHi();
+    }
+
+    
+}
