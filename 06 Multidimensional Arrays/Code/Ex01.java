7e7ee3520a8a9e4f7b5af0446ce336c5d414f679
mazawi <45329653+mazawi@users.noreply.github.com>
2024-02-12 22:30:42 +0400

    1

--- 06 Multidimensional Arrays/Code/Ex01.java
+++ 06 Multidimensional Arrays/Code/Ex01.java
@@ -1,8 +1,12 @@
-public class Code {
+/* Programme to defind a 5X5 array and prints the array */
+
+public class Ex01 
+{
 
     public static void main(String[] args) {
         // Define and initialize a 2D array with numbers
-        int[][] array2D = {
+        int[][] array2D = 
+        {
             {1, 2, 3, 4, 5},
             {6, 7, 8, 9, 10},
             {11, 12, 13, 14, 15},
@@ -11,9 +15,11 @@ public class Code {
         };
 
         // Print the 2D array
-        System.out.println("2D Array:");
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        
+        for (int i = 0; i < 5; i++) 
+        {
+            for (int j = 0; j < 5; j++) 
+            {
                 System.out.print(array2D[i][j] + "\t");
             }
             System.out.println();
