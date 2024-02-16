a66f62b28a9f88f55fe11a4c3449da9e6e549d1c
mazawi <45329653+mazawi@users.noreply.github.com>
2024-02-16 23:46:09 +0400

    1

--- 08 Object-Oriented Programming - Using Classes/Code/Date/Date.java
+++ 08 Object-Oriented Programming - Using Classes/Code/Date/Date.java
@@ -12,27 +12,33 @@ public class Date {
     }
 
     // Getters and setters
-    public int getDay() {
+    public int getDay() 
+    {
         return day;
     }
 
-    public void setDay(int day) {
+    public void setDay(int day) 
+    {
         this.day = day;
     }
 
-    public int getMonth() {
+    public int getMonth() 
+    {
         return month;
     }
 
-    public void setMonth(int month) {
+    public void setMonth(int month) 
+    {
         this.month = month;
     }
 
-    public int getYear() {
+    public int getYear() 
+    {
         return year;
     }
 
-    public void setYear(int year) {
+    public void setYear(int year) 
+    {
         this.year = year;
     }
 
