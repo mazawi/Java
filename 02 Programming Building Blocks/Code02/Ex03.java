6a35b75fa3fdefc87d6c8a112fd26a4e4dbd1c0e
mazawi <45329653+mazawi@users.noreply.github.com>
2024-02-18 01:40:58 +0400

    1

---
+++ 02 Programming Building Blocks/Code02/Ex03.java
@@ -0,0 +1,28 @@
+package COde02;
+
+import java.util.Scanner;
+
+public class Ex03{
+
+    public static void main(String[] args) {
+        
+        int a,b,c,d,e,sum,avg;
+        
+        System.out.println("Please enter five tests score: ");
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        d=sc.nextInt();
+        e=sc.nextInt();
+        
+        sum=a+b+c+d+e;
+        avg=(sum/5);
+        
+        System.out.println("Sum = "+ (double)sum);
+        System.out.println("Average = "+ (double)avg);
+        
+        
+    }
+    
+}
